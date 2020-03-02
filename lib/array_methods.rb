b5a9e4be0cdc43e946cb17df2ff229858a3da272
lively-bundle-0725 <tbandachit@gmail.com>
2020-03-02 21:06:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,9 @@ def find_element_index(array, value_to_find)
   i = 0
   found = nil;
   while i < array.length do
-    if(array[i] == value_to_find){
+    if(array[i] == value_to_find)
       found = i;
-    }
+    end
       i += 1;
     end
     return found
